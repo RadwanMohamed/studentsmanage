8808b3737846b1eb48eefa260de23c063324ac30
RadwanMohamed <radwanmohamed334@gmail.com>
2018-07-10 21:01:31 +0200

    add Doc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Doc</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -14,7 +14,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: black;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -78,18 +78,45 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <h3>this system is divided into two parts</h3>
+                <h3>the first part is the websit:- </h3><br>
+                <p> the task i built is a student registeration form to help student to sign an account 
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                and CRUD system to help the user to edit his information or delete the account
+
+
+                I dont know if you want a system that has admin to control students and add them or edit their information, but if you want that. i can make that easily. 
+
+            </p>
+
+            <h3> the second part is the api for mobile apps</h3>
+            <p> I built that part to help Mobile to make CRUD system in the students and its information</p> <br>
+            <p>
+                the urls {<br>
+                //user<br>
+                1- '/api/students'             :   method=> get    :  return all users in database<br>
+                2- '/api/student/{id}/show',   :   method=> get   : return  user by id<br>
+                3- '/api/student'              :   method=> POST   :  add user <br>
+                4- '/api/student/{id}/delete'  :   method=> get    :  delete user by id<br>
+                5- '/api/student/{id}/update'  :   method=> PUT    :  update user by id<br>
+
+
+                //photos<br>
+
+                1- '/api/photos'               :    method=> get   : return all photos<br>
+                2- '/api/photo/{id}/show',     :    method=> get   : return  photo by id<br>
+                3- '/api/photo/{id}/delete',   :    method=> get   : delete  photo by id<br>
+                4- '/api/photo/{id}/update',   :    method=> PUT   : update  photo by id<br>
+
+            } <br>
+
+            just make sure that you have added ["X-Requested-With":"XMLHttpRequest"] in the header of the request
+            </p>
+
+               
+            
             </div>
+             
         </div>
     </body>
 </html>
